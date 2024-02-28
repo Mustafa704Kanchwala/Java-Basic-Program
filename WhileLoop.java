d518777de9418ee723a9381ac1f7094e35f0e300
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-02-28 21:02:40 +0530

    [ADDED]:Mustafa - This is a example of how While-Loop Works in Java

---
+++ WhileLoop.java
@@ -0,0 +1,15 @@
+class WhileLoop
+{
+	public static void main(String args[])
+	{
+		int i=0;
+		//While loop condition checking
+		while(i < 11)
+		{
+			System.out.println("i= "+i);
+			i++;
+		}
+//loop runs 10-times while the value of i is less than 11//
+
+	}
+}
