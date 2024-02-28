32b6e342c5d5c102bd7162177f1395e4370d2479
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-02-28 20:50:16 +0530

    [ADDED]:Mustafa - This a basic program of Index array in java

---
+++ IndexArray.java
@@ -0,0 +1,12 @@
+class IndexArray
+{
+	public static void main(String args[])
+	{
+		int[] arr = {10,20,30,40,50};
+
+		for(int i=0; i<5; i++)
+		{
+			System.out.println("Index[" + i + "]=" +arr[i]);
+		}
+	}
+}
