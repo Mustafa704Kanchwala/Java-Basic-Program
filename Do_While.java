b0cccf56cd45c69c93826cac29b5adabe5393263
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-02-28 21:41:08 +0530

    [ADDED]:Mustafa- This is a Example how Do-While Loop works

---
+++ Do_While.java
@@ -0,0 +1,17 @@
+class Do_While
+{
+	public static void main(String args[])
+	{
+		int i=12;
+
+		//Do-While Loop:- execute itself once whether the given condition is False //
+
+			do
+			{
+				System.out.println("[i]= "+i);
+				i++;
+		}
+			while(i<11);
+	}
+
+}
